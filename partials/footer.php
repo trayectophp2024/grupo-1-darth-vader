b800d2a137fe6d2c40e12e25e1e6a863ed0ba5b1
nicosolano <nicoesolano@gmail.com>
2024-10-01 04:27:29 -0300

    header + footer

---
+++ partials/footer.php
@@ -0,0 +1,26 @@
+    <!-- FOOTER -->
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-4 d-flex justify-content-center align-items-center">
+                    <a href="index.php">
+                        <img width="200px" src="bosquejos/Logo/Logo SW alt.png" alt="Logo Star Wars">
+                    </a>
+                </div>
+                <div class="col-4 d-flex justify-content-center align-items-end">
+                    <span id="copy">Copyright &copy; 2024</span>
+                </div>
+                <div class="col-2">
+                    <h5>créditos</h5>
+                </div>
+                <div class="col-2">
+                    <h5>redes</h5>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ partials/header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Star Wars</title>
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="bosquejos/Logo/Logo FAVICON.png" type="image/x-icon">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/estilos.css">
+    <!-- ÍCONOS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <!-- HEADER -->
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                    <img width="300px" src="bosquejos/Logo/Logo SW alt.png" alt="Logo Star Wars">
+                </a>
+                <form class="container-fluid">
+                    <div class="input-group">
+                        <span class="input-group-text" id="basic-addon1">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </span>
+                        <input type="text" class="form-control">
+                    </div>
+                </form>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">películas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">personajes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">naves</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">sables</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
