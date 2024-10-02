2c620137af88ece7ed7f5c4722f46a21b9b3b287
nicosolano <nicoesolano@gmail.com>
2024-10-02 20:45:02 -0300

    terminando home

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require "partials/header.php" ?>
 
     <main>
-        <section class="presentacion container-fluid mt-5">
+        <section id="presentacion" class="container-fluid mt-5">
             <div class="row">
                 <div class="col-3 mb-5 d-flex flex-column justify-content-between">
                     <img width="120px" class="m-4" src="bosquejos/ImagenesSW-propias/Home/deathstar-home.png" alt="Estrella de la Muerte">
@@ -12,18 +12,73 @@
                     <p class="text-center">"Dedicado a todo aquel que recorriendo el camino de la Fuerza se encuentre."</p>
                 </div>
                 <div class="col-4 d-flex justify-content-start align-items-end">
-                    <img width="300px" class="yoda-home" src="bosquejos/ImagenesSW-propias/Home/yoda-home.png" alt="Yoda">
+                    <img id="yoda-home" src="bosquejos/ImagenesSW-propias/Home/yoda-home.png" alt="Yoda">
                 </div>
             </div>
         </section>
 
-        <section class="peliculas-home container-fluid">
+        <section id="peliculas-home" class="secciones-home container-fluid">
+            <div class="d-flex flex-column justify-content-center align-items-center">
                 <h2 class="text-center">películas</h2>
-                <div class="d-flex justify-content-center">
                     <button type="button" class="btn">explorar</button>
                 </div>
             </div>
         </section>
+
+        <section id="george-lucas" class="container-fluid my-5">
+            <div class="row">
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <img src="bosquejos/ImagenesSW-propias/Home/George-Lucas.jpg" alt="George Lucas">
+                </div>
+                <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+                    <div class="row mx-5 p-3">
+                        <h3>George Lucas, creador y director.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia delectus deserunt accusamus quis quisquam alias, hic placeat esse nulla, reiciendis corrupti quam vero. Accusamus non officiis doloremque, ut eos laudantium.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro et unde delectus reiciendis deleniti aspernatur, aliquam omnis. Voluptatum, dolor, aut doloribus perferendis quaerat ipsa repellendus fuga id cupiditate, earum qui!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="personajes-home" class="secciones-home container-fluid">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h2 class="text-center">personajes</h2>
+                    <button type="button" class="btn">explorar</button>
+                </div>
+            </div>
+        </section>
+
+        <section id="trailers" class="container-fluid my-5">
+            <div class="row d-flex justify-content-center align-items-center py-4">
+                <iframe width="940" height="529" src="https://www.youtube.com/embed/-ul1pef501M" title="Star Wars: La venganza de los Sith - TRÁILER MODERNO (2022)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/urhsYepFqs0" title="Star Wars: El Imperio Contraataca - TRÁILER MODERNO - 4K || (2022)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="940" height="529" src="https://www.youtube.com/embed/EcQKTTwLA-Y" title="Star Wars: El retorno del Jedi - TRÁILER MODERNO | 4K (40 aniversario)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </section>
+
+        <section id="naves-home" class="secciones-home container-fluid">
+            <div class="d-flex flex-column justify-content-center align-items-center">
+                <h2 class="text-center">naves</h2>
+                    <button type="button" class="btn">explorar</button>
+                </div>
+            </div>
+        </section>
+
+        <section id="john-williams" class="container-fluid my-5">
+            <div class="row">
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <img src="bosquejos/ImagenesSW-propias/Home/John-Williams.jpg" alt="John Williams">
+                </div>
+                <div class="col-6 d-flex flex-column justify-content-center align-items-center">
+                    <div class="row mx-5 p-3">
+                        <h3>John Williams, músico y compositor.</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia delectus deserunt accusamus quis quisquam alias, hic placeat esse nulla, reiciendis corrupti quam vero. Accusamus non officiis doloremque, ut eos laudantium.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro et unde delectus reiciendis deleniti aspernatur, aliquam omnis. Voluptatum, dolor, aut doloribus perferendis quaerat ipsa repellendus fuga id cupiditate, earum qui!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
 
 <?php require "partials/footer.php" ?>
